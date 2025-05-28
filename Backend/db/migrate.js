e76ae76ee8d9cc54118ac18dae9169de6c7cf89c
Julian4806 <julianvanhusen@gmail.com>
2025-05-28 13:06:06 +0200

    test

--- Backend/db/migrate.js
+++ Backend/db/migrate.js
@@ -3,7 +3,7 @@ const migrate = async () => {
   const db = new sqlite3.Database("my.db");
 
   db.exec(`
-    CREATE TABLE IF NOT EXISTS users (
+    CREATE TABLE IF NOT EXISTS comments (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       email VARCHAR(128) NOT NULL,
       password VARCHAR(255) NOT NULL
